d2746202c9d45ac137ee00f2b0064ff4462099fe
njacintho <noemi.jacin@gmail.com>
2023-04-15 20:02:04 -0300

    atualização do script

--- criacao_table_veiculos.sql
+++ criacao_table_veiculos.sql
@@ -4,6 +4,10 @@ create table VEICULOS(
 id INT auto_increment, 
 modelo varchar(50) NOT NULL, 
 placa VARCHAR(7) NOT NULL,
+chassi varchar(17) not null,
 ano int NOT NULL, 
 primary key (id)) Engine = InnoDB; 
 ;
+
+alter table veiculos
+ add chassi varchar(17) not null;
